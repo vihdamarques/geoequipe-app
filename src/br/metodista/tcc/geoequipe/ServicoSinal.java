6db33e6f298b14c617c4141b8a43718ec3fbb2a9
Vih Damarques <vihdamarques@gmail.com>
2013-04-03 02:33:22 -0300

    Enviando sinal pegando coordenadas do GPS
    Alterado uma série de rotinas para funcionar melhor com threads

--- src/br/metodista/tcc/geoequipe/ServicoSinal.java
+++ src/br/metodista/tcc/geoequipe/ServicoSinal.java
@@ -1,21 +1,42 @@
 package br.metodista.tcc.geoequipe;
 
-//import br.metodista.tcc.util.API;
+import br.metodista.tcc.util.API;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
 import android.os.IBinder;
+import android.util.Log;
 
-public class ServicoSinal extends Service /*implements Runnable */{
-	/*@Override
-	public void run() {
-		// TODO Auto-generated method stub
-	}*/
+public class ServicoSinal extends Service implements Runnable {
+	private boolean rodar = true;
+	private Context ctx = this;
+	private int counter = 0;
 
+	public void onCreate() {
+		super.onCreate();
+		Log.i("Servico", "Iniciou serviço");
+		API.atualizarPosicao(ctx);
+		Log.i("Servico", "Fazendo requisição de localização");
+		new Thread(this).start();
+	}
+	
 	@Override
-	 public void onStart(Intent intent, int startId) {
-			new EnviarSinal().execute();
-			stopSelf();
-	 }
+	public void run(){
+		while (this.rodar){
+			// Aguarda 30 segundos para o envio
+			try { Thread.sleep(30 * 1000); } catch (InterruptedException e) {}
+			new Thread(new Runnable() {
+			    public void run() {
+			        Log.i("Servico", "Enviando sinal...");
+			        Location coord = API.getCoord(ctx);
+					API.enviarSinal(API.getIMEI(), API.getUser(), coord.getLatitude(), coord.getLongitude());
+					API.atualizarPosicao(ctx);
+			    }
+			}).start();
+			if (++counter == 3) { this.rodar = false; }
+		}
+	}
 
 	@Override
 	public IBinder onBind(Intent arg0) {
--- src/br/metodista/tcc/util/API.java
+++ src/br/metodista/tcc/util/API.java
@@ -10,15 +10,21 @@ import java.util.Date;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.os.StrictMode;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 
+@SuppressLint("SimpleDateFormat")
 public class API {
 	public static void acessaURL(String host, int port, String param) {
-
+		Log.i("Servico", "entrou acessa URL: url => " + host + ":" + port + param);
 		try {
             URL url = new URL(host + ":" + port + param);
-            Log.i("Servico", "url => " + host + ":" + port + param);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setConnectTimeout(60000);
             conn.setReadTimeout(60000);
@@ -33,12 +39,12 @@ public class API {
     }
 
     public static String geraParametros(int imei, int user, double lat, double lng) {
-        SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
         JSONObject json = new JSONObject();
         try {
 	        json.put("imei", String.valueOf(imei));
 	        json.put("user", String.valueOf(user));
-	        json.put("data", fmt.format(new Date()));
+	        json.put("data", new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()));
 	
 	        JSONObject coord = new JSONObject();
 	        coord.put("lat",String.valueOf(lat));
@@ -51,5 +57,60 @@ public class API {
 
         //System.out.println(json.toString());
         return Base64.encodeBytes(json.toString().getBytes());
+    }
+
+    public static String getUrl(){
+    	return "http://10.0.2.2";
+    }
+
+    public static int getPort(){
+    	return 3014;
+    }
+
+    public static int getIMEI(){
+    	//return android.telephony.TelephonyManager.getDeviceId();
+    	return 111111111;
+    }
+
+    public static int getUser(){
+    	return 1;
+    }
+
+    public static Location getCoord(Context ctx){
+    	final String locProvider = LocationManager.GPS_PROVIDER; //NETWORK_PROVIDER
+    	LocationManager lm = (LocationManager) ctx.getSystemService(Context.LOCATION_SERVICE);
+    	Location coord = lm.getLastKnownLocation(locProvider);
+    	return coord;
+    }
+
+    public static void enviarSinal(int imei, int user, double lat, double lng) {
+    	acessaURL(getUrl(), getPort(), "/sinal/" + geraParametros(imei, user, lat, lng));
+    }
+
+    public static void atualizarPosicao(final Context ctx){
+    	new Thread(new Runnable() {
+		    public void run() {
+		    	Looper.prepare();
+		    	final String locProvider = LocationManager.GPS_PROVIDER; //NETWORK_PROVIDER
+		    	LocationManager lm = (LocationManager) ctx.getSystemService(Context.LOCATION_SERVICE);
+		    	LocationListener ll = new LocationListener() {
+		    		@Override
+		            public void onLocationChanged(final Location coord) {
+		            	Log.i("Servico", "Atualizou posição");
+		            	LocationManager lm = (LocationManager) ctx.getSystemService(Context.LOCATION_SERVICE);
+		            	lm.removeUpdates(this);
+		            }
+		            @Override
+		            public void onStatusChanged(final String provider, final int status, final Bundle extras) {}
+		            @Override
+		            public void onProviderEnabled(final String provider) {}
+		            @Override
+		            public void onProviderDisabled(final String provider) {}
+		        };
+		        lm.requestLocationUpdates(locProvider, 0, 0, ll);
+		        Looper.loop();
+		        Looper.myLooper().quit();
+		    }
+		}).start();
     }
 }
