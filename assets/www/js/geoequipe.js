d5779b66ea4698104aeb13a9bebb5b82a44c55e9
Samanta Bueno da Silva <samantabueno@gmail.com>
2013-07-30 22:52:54 -0300

    telas com login

--- assets/www/js/geoequipe.js
+++ assets/www/js/geoequipe.js
@@ -2,17 +2,21 @@ function set_sessao(){
 	var login = $("input[id=textinput1]").val();
 	localStorage.setItem('usuario',login);
 };
-		
+	
 function get_sessao(){
 	alert(localStorage.getItem('usuario'));
 };
-		
-		
+
 function limpa_sessao(){
 	localStorage.setItem('usuario',null);
 };
 
-function login(){
-	set_sessao();
-	window.location = "lista_tarefas.html";
+function fnc_login(){
+	var login = $("input[id=textinput1]").val();
+	var senha = $("input[id=textinput2]").val();
+    if (login == "admin" && senha == "123") {
+		window.location = "lista_tarefas.html";
+	}else{
+		alert('Login ou Senha Invalidos!');
+	}
 };
